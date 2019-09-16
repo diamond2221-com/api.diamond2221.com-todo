539e7daee76a85e015308f5b187971f8cd27b3ab
张毓 <zhangyu@wyc.cn>
2019-09-16 12:11:02 +0800

    线上数据库连接

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,6 +2,25 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
-
+  config.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: '127.0.0.1',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      // password: '981220zy',  /* 家用 */
+      password: '981220Zy+++', /* 公司 */
+      // 数据库名
+      database: 'db_instagram',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  }
   return config;
 };
