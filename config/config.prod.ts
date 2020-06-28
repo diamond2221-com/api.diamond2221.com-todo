86d885d9a4b0c0c365fc595bfe51bf9b71de17cf
张毓 <diamond2221@163.com>
2020-06-28 23:01:27 +0800

    设置为*

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -51,7 +51,7 @@ export default () => {
     };
 
     config.cors = {
-        origin: 'https://m.diamond2221.com,https://www.diamond2221.com,https://api.diamond2221.com,https://blog.diamond2221.com', // 可以填入 '*'，如果你不使用cookie的话。
+        origin: '*', // 可以填入 '*'，如果你不使用cookie的话。
         credentials: true, // 允许cookie跨域
         allowMethods: 'GET,HEAD,PUT,POST,DELTE,PATCH,OPTIONS',
     };
