5137f36d8bd7811658f55319425646daadd3f472
diamond2221 <41322627+diamond2221@users.noreply.github.com>
2020-09-28 09:05:55 +0800

    Update context.ts

--- app/extend/context.ts
+++ app/extend/context.ts
@@ -1,18 +1,4 @@
-import * as Core from '@alicloud/pop-core'
 import { Context } from '../../typings/app';
-
-const AliClient = new Core({
-    accessKeyId: 'LTAInaRxcKVhzEN3',
-    accessKeySecret: 'DYsHsKmn7Ovt8K26M8dlrG82cnsgcb',
-    endpoint: 'https://dysmsapi.aliyuncs.com',
-    apiVersion: '2017-05-25'
-});
-interface ISms {
-    phoneNumbers: string, /* 接收短信的手机号码。 多个用 , 隔开 */
-    SignName: string, /* 短信签名名称。请在控制台签名管理页面签名名称一列查看。 */
-    TemplateCode: string /* 短信模板ID。请在控制台模板管理页面模板CODE一列查看。 */
-    Code: string
-}
 
 module.exports = {
 
@@ -55,42 +41,6 @@ module.exports = {
 
             }
         }
-    },
-
-    /**
-     * @description
-     * @author ZhangYu
-     * @date 2020-02-18
-     * @param {ISms} data
-     */
-    sendSms(this: Context, data: ISms) {
-
-        var params = {
-            "RegionId": "cn-hangzhou",
-            "PhoneNumbers": data.phoneNumbers,
-            "SignName": data.SignName,
-            "TemplateCode": data.TemplateCode,
-            "TemplateParam": JSON.stringify({ code: data.Code })
-        }
-
-        var requestOption = {
-            method: 'POST'
-        };
-
-        return new Promise((resolve, reject) => {
-            return AliClient.request('SendSms', params, requestOption).then(res => {
-                resolve(res)
-            }).catch(err => {
-                reject(err);
-                this.send('你的操作太频繁了，请稍后再试', 400)
-                this.app.logger.error(err);
-            })
-        })
-    },
-    sendSms1(data: ISms) {
-        return new Promise((resolve) => {
-            resolve(data);
-        })
     },
     getUid(this: Context) {
         return this.request.header["client-uid"]
