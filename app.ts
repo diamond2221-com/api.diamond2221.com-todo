1162adb1828ac96ab6c9b97f26b6ba8484a9c2fd
张毓 <zhangyu@shanghe.link>
2020-09-29 15:43:39 +0800

    取消注释

--- app.ts
+++ app.ts
@@ -2,5 +2,7 @@ import { Application } from "egg";
 import "reflect-metadata"
 
 export default async (app: Application) => {
+    // await app.graphql.init();
+
     app.logger.info("Server Started!")
 }
