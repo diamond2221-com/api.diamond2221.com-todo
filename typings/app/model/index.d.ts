3649f6d3436d97799c17587d170d82c2e11039c5
张毓 <zhangyu@shanghe.link>
2021-02-20 10:39:18 +0800

    还原之前

--- typings/app/model/index.d.ts
+++ typings/app/model/index.d.ts
@@ -3,11 +3,15 @@
 
 import 'egg';
 import ExportTodo from '../../../app/model/todo';
-import ExportUser from '../..../../../test/todomodel
+import ExportTodomodel from '../../../app/model/todomodel';
+import ExportUser from '../../../app/model/user';
+import ExportUsermodel from '../../../app/model/usermodel';
 
 declare module 'egg' {
   interface IModel {
     Todo: ReturnType<typeof ExportTodo>;
+    Todomodel: ReturnType<typeof ExportTodomodel>;
     User: ReturnType<typeof ExportUser>;
+    Usermodel: ReturnType<typeof ExportUsermodel>;
   }
 }
